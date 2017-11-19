1c0d2e0ae4df697e6351b3af6203a87b5b0980b4
Pablo Sancho-Sopranis <psanchosopranis@gmail.com>
2017-11-19 12:12:46 +0100

    Archivo agregado vía upload
    Archivo de Script Bash para arranque/re-arranque del Nodo Ethereum creado en $HOME/GETHDATOS del ejemplo

---
+++ arranque_nodo_en_GETHDATOS.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ============================================
+# Script de Inicio del NODO Ethereum
+# ============================================
+set -x
+geth --identity "test00" \
+--mine --minerthreads=4 \
+--datadir /home/devel1/GETHDATOS/ \
+--port 30310 --rpc --rpcport 8110 \
+--networkid 4567890 \
+--nodiscover --maxpeers 0 \
+--vmdebug --verbosity 6 \
+--pprof --pprofport 6110
+set +x
